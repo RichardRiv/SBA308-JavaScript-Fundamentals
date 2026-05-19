0157e2a307e88a07a9c06ac4559296cf617207f0
RichardRiv <r.richardev@gmail.com>
2026-05-19 18:09:19 -0400

    Reformatted Code

--- script.js
+++ script.js
@@ -77,14 +77,14 @@ const LearnerSubmissions = [
 ];
 
 const isCourseIdMatched = (course, ag) => {
-	let courseId = course.id;
-	let agCourseId = ag.course_id;
+	const courseId = course.id;
+	const agCourseId = ag.course_id;
 
 	try {
 		if (courseId === agCourseId) {
 			return true;
 		} else {
-			return new Error(
+			throw new Error(
 				'Assignment Group Course ID and Course Info ID do not match.',
 			);
 		}
@@ -99,8 +99,8 @@ const filterByPassedDueDate = (ag) => {
 };
 
 const groupDataByLid = (submissions, ag) => {
-	let filteredAssignments = filterByPassedDueDate(ag);
-	let rel = {};
+	const filteredAssignments = filterByPassedDueDate(ag);
+	const rel = {};
 
 	for (let sub of submissions) {
 		let lid = sub.learner_id;
@@ -116,7 +116,7 @@ const groupDataByLid = (submissions, ag) => {
 
 const convertToObjects = (ag) => {
 	const filteredAssignments = filterByPassedDueDate(ag);
-	let assignmentsObject = {};
+	const assignmentsObject = {};
 
 	for (let obj of filteredAssignments) {
 		assignmentsObject[obj.id] = obj;
@@ -131,7 +131,7 @@ const countDecimalPlaces = (grade) => {
 };
 
 const formatDecimalGrade = (grade) => {
-	let decCount = countDecimalPlaces(grade);
+	const decCount = countDecimalPlaces(grade);
 
 	if (decCount === 0) {
 		return Number(grade.toFixed(1));
@@ -142,6 +142,36 @@ const formatDecimalGrade = (grade) => {
 	}
 };
 
+const buildFinalResult = (filteredAssignments, filteredAssignmentsObj) => {
+	const result = [];
+
+	for (let learner in filteredAssignments) {
+		let learnerObj = { id: Number(learner) };
+		let average = 0;
+		let pointsPossible = 0;
+
+		for (let obj of filteredAssignments[learner]) {
+			let assignment = filteredAssignmentsObj[obj.assignment_id];
+			let score = obj.submission.score;
+			let isLate =
+				new Date(obj.submission.submitted_at) > new Date(assignment.due_at);
+
+			if (isLate) score -= assignment.points_possible * 0.1;
+
+			let assignmentGrade = score / assignment.points_possible;
+			assignmentGrade = Number(formatDecimalGrade(assignmentGrade));
+			learnerObj[assignment.id] = assignmentGrade;
+
+			average += score;
+			pointsPossible += assignment.points_possible;
+		}
+		learnerObj['avg'] = average / pointsPossible;
+		result.push(learnerObj);
+	}
+
+	return result;
+};
+
 function getLearnerData(course, ag, submissions) {
 	const isForSameCourse = isCourseIdMatched(course, ag);
 
@@ -149,59 +179,8 @@ function getLearnerData(course, ag, submissions) {
 		const filteredAssignments = groupDataByLid(submissions, ag);
 		const filteredAssignmentsObj = convertToObjects(AssignmentGroup);
 
-		let result = [];
-		for (let learner in filteredAssignments) {
-			let learnerObj = { id: Number(learner) };
-			let average = 0;
-			let pointsPossible = 0;
-
-			for (let obj of filteredAssignments[learner]) {
-				let assignment = filteredAssignmentsObj[obj.assignment_id];
-				let score = obj.submission.score;
-				let isLate =
-					new Date(obj.submission.submitted_at) > new Date(assignment.due_at);
-
-				if (isLate) score -= assignment.points_possible * 0.1;
-
-				let assignmentGrade = score / assignment.points_possible;
-				assignmentGrade = Number(formatDecimalGrade(assignmentGrade));
-				learnerObj[assignment.id] = assignmentGrade;
-
-				average += score;
-				pointsPossible += assignment.points_possible;
-			}
-			learnerObj['avg'] = average / pointsPossible;
-			result.push(learnerObj);
-		}
-
-		console.log(result);
-
-		/**
-		 * obj = {
-		 *   id: learner_id,
-		 *   avg: (submission.score + submission.score) / (ag.a.pts_possible + ag.a.pts_possible)
-		 *   ag.a.id: submission.score / ag.a.pts_possible
-		 *   ag.a.id: submission.score / ag.a.pts_possible
-		 *   ag.a.id: (submission.score - 10%) / ag.a.pts_possible IF LATE
-		 * }
-		 */
+		return buildFinalResult(filteredAssignments, filteredAssignmentsObj);
 	}
-	// // here, we would process this data to achieve the desired result.
-	// const result = [
-	// 	{
-	// 		id: 125,
-	// 		avg: 0.985, // (47 + 150) / (50 + 150)
-	// 		1: 0.94, // 47 / 50
-	// 		2: 1.0, // 150 / 150
-	// 	},
-	// 	{
-	// 		id: 132,
-	// 		avg: 0.82, // (39 + 125) / (50 + 150)
-	// 		1: 0.78, // 39 / 50
-	// 		2: 0.833, // late: (140 - 15) / 150
-	// 	},
-	// ];
-	// return result;
 }
 
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
